01b4cfad52d0a695c4deca5da67134beaf0b0eac
seniorpung <pungsurich@proton.me>
2024-11-03 07:47:17 +0800

    Use an image picker

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,10 +4,10 @@ import { StyleSheet, View, Pressable, Text } from 'react-native';
 type Props = {
   label: string;
   theme?: 'primary';
+  onPress?: () => void;
 };
 
-export default function Button({ label, /* @tutinfo The prop <CODEtheme</CODE> to detect the button variant. */theme }: Props) {
-  /* @tutinfo Conditionally render the primary themed button. */
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === 'primary') {
     return (
       <View
@@ -17,7 +17,7 @@ export default function Button({ label, /* @tutinfo The prop <CODEtheme</CODE> t
         ]}>
         <Pressable
           style={[styles.button, { backgroundColor: '#fff' }]}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={onPress}>
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
